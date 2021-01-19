496781519175cf6c645a6854f1f585d45f1d734c
Angel Cardenas <maskemaneac@gmail.com>
2021-01-18 22:39:57 -0600

    Initial transcription from YouTube video

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {SidebarContainer, Icon, CloseIcon, SiderbarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute} from './SidebarElements';
+
+const Sidebar = ({isOpen, toggle}) => {
+ return (
+  <SidebarContainer isOpen={isOpen} onClick={toggle}>
+    <Icon onClick={toggle}>
+     <CloseIcon/>
+    </Icon>
+    <SiderbarWrapper>
+     <SidebarMenu>
+      <SidebarLink to="about" onClick={toggle}>
+       About
+      </SidebarLink>
+      <SidebarLink to="discover" onClick={toggle}>
+       Discover
+      </SidebarLink>
+      <SidebarLink to="services" onClick={toggle}>
+       Services
+      </SidebarLink>
+      <SidebarLink to="signup" onClick={toggle}>
+       Sign Up
+      </SidebarLink>
+     </SidebarMenu>
+     <SideBtnWrap>
+      <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+     </SideBtnWrap>
+    </SiderbarWrapper>
+  </SidebarContainer>
+ )
+}
+
+export default Sidebar;
